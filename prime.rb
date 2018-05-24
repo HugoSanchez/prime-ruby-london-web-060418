5857d9a351ee31410d50e285b875a8ed795df001
Hugo Sanchez <hugosanchez251@gmail.com>
2018-05-24 15:43:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
-def prime?(x):
+def prime?(x)
     if x < 2:
         return False
-    else:
-        for n in range(2,x):
-            if x % n == 0:
-               return False
-            else return True
+    else
+        for n in (2..x)
+            if x % n == 0
+               return false
+            else return true
       end
     end
   end
